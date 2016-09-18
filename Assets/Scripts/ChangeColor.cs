5eecdabef7f0e6c1de5a6a3a20bf099ba20bebcb
rosiezou <rosiezou@gmail.com>
2016-09-17 22:49:14 -0400

    change of color script

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+    public float TotalTimelapse = 5.0f;
+    public float Interval = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Interval = Time.deltaTime;
+        if (Interval <= 0.0f)
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
