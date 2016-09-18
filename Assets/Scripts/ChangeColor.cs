0e14e0ce763023707924cc26f31bb355fb30029a
rosiezou <rosiezou@gmail.com>
2016-09-18 05:35:58 -0400

    FINALLY GOT THE GRADIENT COLOR RIGHT

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -2,12 +2,21 @@
 using System.Collections;
 
 public class ChangeColor : MonoBehaviour {
-    public float TotalTimelapse = 5.0f;
+    public float TotalTimelapse = 10.0f;
     public float Interval = 1.0f;
-    public float red = 0.3f;
-    public float green = 2.0f;
-    public float blue = 1.0f;
-    public float transparency = 0.0f;
+    public float initRed = 0.0f;
+    public float initGreen = 0.0f;
+    public float initBlue = 1.0f;
+
+    public float red = 0.0f;
+    public float green = 0.0f;
+    public float blue = 0.0f;
+
+    public float transparency = 1.0f;
+    public float finalRed = 1.0f;
+    public float finalBlue = 0.0f;
+    public float finalGreen = 1.0f;
+    public float totalTime = 0.0f;
 	// Use this for initialization
 	void Start () {
         //need a trigger
@@ -20,10 +29,19 @@ public class ChangeColor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        red = Time.deltaTime * 20f;
-        green = Time.deltaTime * 30f;
-        blue = Time.deltaTime * 50f;
-        transparency = Time.deltaTime * 10f;
+        if (totalTime > 5.0f) return;
+        float t = Time.deltaTime;
+        Debug.Log(t);
+        totalTime += t;
+        Debug.Log(totalTime);
+        red = ((1.0f - (totalTime / TotalTimelapse)) * initRed) + ((totalTime / TotalTimelapse) * finalRed);
+        green = ((1.0f - (totalTime / TotalTimelapse)) * initGreen) + ((totalTime / TotalTimelapse) * finalGreen);
+        blue = ((1.0f - (totalTime / TotalTimelapse)) * initBlue) + ((totalTime / TotalTimelapse) * finalBlue);
+        transparency = 1.0f;
+        Debug.Log(red);
+        Debug.Log(green);
+        Debug.Log(blue);
+        Debug.Log(transparency);
         gameObject.GetComponent<Renderer>().material.color = new Vector4(red, green, blue, transparency);
         }
     }
